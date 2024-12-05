e73f0a532189de4ca2799829f9d6d366907b2204
rndlwjs <rndlwjs@gmail.com>
2024-12-05 15:35:45 +0900

    feat: adding branchformer, not finished

---
+++ main/branchformer/eb.py
@@ -0,0 +1,258 @@
+import logging
+from typing import Optional, Tuple
+import torch
+from branchformer.nets_utils import make_pad_mask, get_activation, Swish
+from branchformer.subsampling import Conv2dSubsampling
+from branchformer.embedding import RelPositionalEncoding
+from branchformer.eb import EBranchformerEncoderLayer
+from branchformer.attention import RelPositionMultiHeadedAttention
+from branchformer.cgmlp import ConvolutionalGatingMLP
+from branchformer.repeat import repeat
+from branchformer.layer_norm import LayerNorm
+
+class EBranchformerEncoderLayer(torch.nn.Module):
+    """E-Branchformer encoder layer module.
+
+    Args:
+        size (int): model dimension
+        attn: standard self-attention or efficient attention
+        cgmlp: ConvolutionalGatingMLP
+        feed_forward: feed-forward module, optional
+        feed_forward: macaron-style feed-forward module, optional
+        dropout_rate (float): dropout probability
+        merge_conv_kernel (int): kernel size of the depth-wise conv in merge module
+    """
+
+    def __init__(
+        self,
+        size: int,
+        attn: torch.nn.Module,
+        cgmlp: torch.nn.Module,
+        feed_forward: Optional[torch.nn.Module],
+        feed_forward_macaron: Optional[torch.nn.Module],
+        dropout_rate: float,
+        merge_conv_kernel: int = 3,
+    ):
+        super().__init__()
+
+        self.size = size
+        self.attn = attn
+        self.cgmlp = cgmlp
+
+        self.feed_forward = feed_forward
+        self.feed_forward_macaron = feed_forward_macaron
+        self.ff_scale = 1.0
+        if self.feed_forward is not None:
+            self.norm_ff = LayerNorm(size)
+        if self.feed_forward_macaron is not None:
+            self.ff_scale = 0.5
+            self.norm_ff_macaron = LayerNorm(size)
+
+        self.norm_mha = LayerNorm(size)  # for the MHA module
+        self.norm_mlp = LayerNorm(size)  # for the MLP module
+        self.norm_final = LayerNorm(size)  # for the final output of the block
+
+        self.dropout = torch.nn.Dropout(dropout_rate)
+
+        self.depthwise_conv_fusion = torch.nn.Conv1d(
+            size + size,
+            size + size,
+            kernel_size=merge_conv_kernel,
+            stride=1,
+            padding=(merge_conv_kernel - 1) // 2,
+            groups=size + size,
+            bias=True,
+        )
+        self.merge_proj = torch.nn.Linear(size + size, size)
+
+    def forward(self, x_input, mask, cache=None):
+        """Compute encoded features.
+
+        Args:
+            x_input (Union[Tuple, torch.Tensor]): Input tensor w/ or w/o pos emb.
+                - w/ pos emb: Tuple of tensors [(#batch, time, size), (1, time, size)].
+                - w/o pos emb: Tensor (#batch, time, size).
+            mask (torch.Tensor): Mask tensor for the input (#batch, 1, time).
+            cache (torch.Tensor): Cache tensor of the input (#batch, time - 1, size).
+        Returns:
+            torch.Tensor: Output tensor (#batch, time, size).
+            torch.Tensor: Mask tensor (#batch, time).
+        """
+
+        if cache is not None:
+            raise NotImplementedError("cache is not None, which is not tested")
+
+        if isinstance(x_input, tuple):
+            x, pos_emb = x_input[0], x_input[1]
+        else:
+            x, pos_emb = x_input, None
+
+        if self.feed_forward_macaron is not None:
+            residual = x
+            x = self.norm_ff_macaron(x)
+            x = residual + self.ff_scale * self.dropout(self.feed_forward_macaron(x))
+
+        # Two branches
+        x1 = x
+        x2 = x
+
+        # Branch 1: multi-headed attention module
+        x1 = self.norm_mha(x1)
+
+        #if isinstance(self.attn, FastSelfAttention):
+        #    x_att = self.attn(x1, mask)
+        if True: #else:
+            if pos_emb is not None:
+                x_att = self.attn(x1, x1, x1, pos_emb, mask)
+            else:
+                x_att = self.attn(x1, x1, x1, mask)
+
+        x1 = self.dropout(x_att)
+
+        # Branch 2: convolutional gating mlp
+        x2 = self.norm_mlp(x2)
+
+        if pos_emb is not None:
+            x2 = (x2, pos_emb)
+        x2 = self.cgmlp(x2, mask)
+        if isinstance(x2, tuple):
+            x2 = x2[0]
+
+        x2 = self.dropout(x2)
+
+        # Merge two branches
+        x_concat = torch.cat([x1, x2], dim=-1)
+        x_tmp = x_concat.transpose(1, 2)
+        x_tmp = self.depthwise_conv_fusion(x_tmp)
+        x_tmp = x_tmp.transpose(1, 2)
+        x = x + self.dropout(self.merge_proj(x_concat + x_tmp))
+
+        if self.feed_forward is not None:
+            # feed forward module
+            residual = x
+            x = self.norm_ff(x)
+            x = residual + self.ff_scale * self.dropout(self.feed_forward(x))
+
+        x = self.norm_final(x)
+
+        if pos_emb is not None:
+            return (x, pos_emb), mask
+
+        return x, mask
+
+class EBranchformerEncoder(AbsEncoder):
+    """E-Branchformer encoder module."""
+
+    @typechecked
+    def __init__(
+        self,
+        input_size: int = 80,
+        output_size: int = 256,
+        attention_heads: int = 4,
+        attention_layer_type: str = "rel_selfattn",
+        pos_enc_layer_type: str = "rel_pos",
+        rel_pos_type: str = "latest",
+        cgmlp_linear_units: int = 2048,
+        cgmlp_conv_kernel: int = 31,
+        use_linear_after_conv: bool = False,
+        gate_activation: str = "identity",
+        num_blocks: int = 12,
+        dropout_rate: float = 0.1,
+        positional_dropout_rate: float = 0.1,
+        attention_dropout_rate: float = 0.0,
+        input_layer: Optional[str] = "conv2d",
+        zero_triu: bool = False,
+        padding_idx: int = -1,
+        layer_drop_rate: float = 0.0,
+        max_pos_emb_len: int = 5000,
+        use_ffn: bool = False,
+        macaron_ffn: bool = False,
+        ffn_activation_type: str = "swish",
+        linear_units: int = 2048,
+        positionwise_layer_type: str = "linear",
+        merge_conv_kernel: int = 3,
+        interctc_layer_idx=None,
+        interctc_use_conditioning: bool = False,
+        qk_norm: bool = False,
+        use_flash_attn: bool = True,
+    ):
+        super().__init__()
+        self._output_size = output_size
+
+        ### RelPosEnc
+        self.pos_enc_class = RelPositionalEncoding
+
+        ### Conv2dSubsampling
+        self.embed = Conv2dSubsampling(
+            idim=80, 
+            odim=config['output_size'], 
+            dropout_rate=['dropout_rate'], 
+            pos_enc=pos_enc_class(d_model=config['output_size'], dropout_rate=config['positional_dropout_rate'], max_len=5000)
+            )
+
+        self.activation = get_activation(ffn_activation_type)
+
+        ### MHSA
+        self.encoder_selfattn_layer = RelPositionMultiHeadedAttention
+        self.encoder_selfattn_layer_args = (config['attention_heads'], config['output_size'], config['dropout_rate'], False) #n_head, n_feat, dropout_rate, zero_triu=False
+
+        ### CGMLP
+        self.cgmlp_layer = ConvolutionalGatingMLP
+        self.cgmlp_layer_args = (config['output_size'], config['cgmlp_linear_units'], config['cgmlp_conv_kernel'], config['dropout_rate'], False, config['gate_activation'],)
+
+        self.positionwise_layer = PositionwiseFeedForward
+        self.positionwise_layer_args = (output_size, linear_units, dropout_rate, activation,)
+
+        self.encoders = repeat(
+            config['num_blocks'],
+            lambda lnum: EBranchformerEncoderLayer(
+                size=config['output_size'],
+                attn=encoder_selfattn_layer(*encoder_selfattn_layer_args),
+                cgmlp=cgmlp_layer(*cgmlp_layer_args),
+                feed_forward=positionwise_layer(*positionwise_layer_args) if use_ffn else None,
+                feed_forward_macaron=(
+                    positionwise_layer(*positionwise_layer_args)
+                    if use_ffn and macaron_ffn
+                    else None
+                ),
+                dropout_rate=config['dropout_rate'],
+                merge_conv_kernel=config['merge_conv_kernel'],
+            ),
+            config['layer_drop_rate'],
+        )
+        self.after_norm = LayerNorm(output_size)
+
+    def forward(
+        self,
+        xs_pad: torch.Tensor,
+        ilens: torch.Tensor,
+        prev_states: torch.Tensor = None,
+        ctc: CTC = None,
+        max_layer: int = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor]]:
+        """Calculate forward propagation.
+
+        Args:
+            xs_pad (torch.Tensor): Input tensor (#batch, L, input_size).
+            ilens (torch.Tensor): Input length (#batch).
+            prev_states (torch.Tensor): Not to be used now.
+            ctc (CTC): Intermediate CTC module.
+            max_layer (int): Layer depth below which InterCTC is applied.
+        Returns:
+            torch.Tensor: Output tensor (#batch, L, output_size).
+            torch.Tensor: Output length (#batch).
+            torch.Tensor: Not to be used now.
+        """
+
+        masks = (~make_pad_mask(ilens)[:, None, :]).to(xs_pad.device)
+        xs_pad, masks = self.embed(xs_pad, masks)
+
+        xs_pad, masks = self.encoders(xs_pad, masks)
+
+        if isinstance(xs_pad, tuple):
+            xs_pad = xs_pad[0]
+
+        xs_pad = self.after_norm(xs_pad)
+        olens = masks.squeeze(1).sum(1)
+
+        return xs_pad, olens, None
---
+++ main/branchformer/test.py
@@ -0,0 +1,112 @@
+import yaml
+import torch
+from nets_utils import make_pad_mask, get_activation, Swish
+from subsampling import Conv2dSubsampling
+from embedding import RelPositionalEncoding
+from eb import EBranchformerEncoderLayer
+from attention import RelPositionMultiHeadedAttention
+from cgmlp import ConvolutionalGatingMLP
+from repeat import repeat
+from layer_norm import LayerNorm
+
+### Config
+
+path = 'train_asr_e_branchformer_size256_mlp1024_linear1024_e12_mactrue_edrop0.0_ddrop0.0.yaml'
+with open(path) as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)['encoder_conf']
+
+### start here
+
+xs_pad = torch.rand(1, 80, 200)
+xs_pad = xs_pad.transpose(1, 2) #입력은 [B, T, F] 이다.
+ilens = torch.LongTensor([200])
+
+masks = (~make_pad_mask(ilens)[:, None, :]).to(xs_pad.device)
+
+### RelPosEnc
+pos_enc_class = RelPositionalEncoding
+
+### subsampling
+
+embed = Conv2dSubsampling(idim=80, 
+                          odim=config['output_size'], 
+                          dropout_rate=['dropout_rate'], 
+                          pos_enc=pos_enc_class(d_model=config['output_size'], dropout_rate=config['positional_dropout_rate'], max_len=5000)
+                          )
+
+xs_pad, masks = embed(xs_pad, masks)
+
+### MHSA
+encoder_selfattn_layer = RelPositionMultiHeadedAttention
+encoder_selfattn_layer_args = (config['attention_heads'], config['output_size'], config['dropout_rate'], False) #n_head, n_feat, dropout_rate, zero_triu=False
+
+### CGMLP
+cgmlp_layer = ConvolutionalGatingMLP
+cgmlp_layer_args = (config['output_size'], config['cgmlp_linear_units'], config['cgmlp_conv_kernel'], config['dropout_rate'], False, config['gate_activation'],)
+
+### FFN
+class PositionwiseFeedForward(torch.nn.Module):
+    """Positionwise feed forward layer.
+
+    Args:
+        idim (int): Input dimenstion.
+        hidden_units (int): The number of hidden units.
+        dropout_rate (float): Dropout rate.
+
+    """
+
+    def __init__(self, idim, hidden_units, dropout_rate, activation=torch.nn.ReLU()):
+        """Construct an PositionwiseFeedForward object."""
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = torch.nn.Linear(idim, hidden_units)
+        self.w_2 = torch.nn.Linear(hidden_units, idim)
+        self.dropout = torch.nn.Dropout(dropout_rate)
+        self.activation = activation
+
+    def forward(self, x):
+        """Forward function."""
+        return self.w_2(self.dropout(self.activation(self.w_1(x))))
+
+positionwise_layer = PositionwiseFeedForward
+positionwise_layer_args = (config['output_size'], config['linear_units'], config['positional_dropout_rate'], get_activation('swish'))
+
+### encoder
+use_ffn = config['use_ffn']
+macaron_ffn = config['macaron_ffn']
+
+encoders = repeat(
+    config['num_blocks'],
+    lambda lnum: EBranchformerEncoderLayer(
+        size=config['output_size'],
+        attn=encoder_selfattn_layer(*encoder_selfattn_layer_args),
+        cgmlp=cgmlp_layer(*cgmlp_layer_args),
+        feed_forward=positionwise_layer(*positionwise_layer_args) if use_ffn else None,
+        feed_forward_macaron=(
+            positionwise_layer(*positionwise_layer_args)
+            if use_ffn and macaron_ffn
+            else None
+        ),
+        dropout_rate=config['dropout_rate'],
+        merge_conv_kernel=config['merge_conv_kernel'],
+    ),
+    config['layer_drop_rate'],
+)
+
+from pytorch_lightning.utilities.model_summary import LayerSummary
+
+param = LayerSummary(encoders).num_parameters / 1000000
+print(param); exit()
+
+xs_pad, masks = encoders(xs_pad, masks)
+
+if isinstance(xs_pad, tuple):
+    xs_pad = xs_pad[0]
+
+### LayerNorm
+LayerNorm = LayerNorm(256)
+
+xs_pad = LayerNorm(xs_pad)
+olens = masks.squeeze(1).sum(1)
+
+print(xs_pad.shape, olens.shape)
+print(olens)
